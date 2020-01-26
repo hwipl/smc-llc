e211fc2a48499d73b76435d1188c07c7ba2fb31a
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-26 15:02:28 +0100

    add initial rocev1 and rocev2 detection
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- llc.go
+++ llc.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
@@ -19,6 +20,33 @@ var (
 
 // parse parses packet
 func parse(packet gopacket.Packet) {
+	// packet must be ethernet
+	ethLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethLayer == nil {
+		return
+	}
+
+	// RoCEv1
+	eth, _ := ethLayer.(*layers.Ethernet)
+	if eth.EthernetType == 0x8915 {
+		fmt.Printf("%s RoCEv1 Packet:\n",
+			packet.Metadata().Timestamp)
+		fmt.Println(packet.Dump())
+		return
+	}
+
+	// RoCEv2
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return
+	}
+	udp, _ := udpLayer.(*layers.UDP)
+	if udp.DstPort == 4791 {
+		fmt.Printf("%s RoCEv2 Packet:\n",
+			packet.Metadata().Timestamp)
+		fmt.Println(packet.Dump())
+		return
+	}
 }
 
 // listen captures packets on the network interface and parses them
