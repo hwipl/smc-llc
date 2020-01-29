4c2886088e57e7aac81efe6f2a54e4580e3ef715
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-29 17:08:43 +0100

    add types addLinkCont and rkeyPair
    Add types for add link continuation messages and rkey/rtoken pairs and
    use them.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- llc.go
+++ llc.go
@@ -250,70 +250,112 @@ func parseAddLink(buffer []byte) {
 	fmt.Println(add)
 }
 
-// parseAddLinkCont parses the LLC add link continuation message in buffer
-func parseAddLinkCont(buffer []byte) {
-	// Message type is 1 byte
-	typ := buffer[0]
-	buffer = buffer[1:]
+// rkeyPair stores a RKey/RToken pair
+type rkeyPair struct {
+	referenceRKey uint32
+	newRKey       uint32
+	newVAddr      uint64
+}
 
-	// Message length is 1 byte, should be equal to 44
-	length := buffer[0]
-	buffer = buffer[1:]
-
-	// Reserved 1 byte
-	res1 := buffer[0]
-	buffer = buffer[1:]
-
-	// Reply is first bit in this byte
-	reply := (buffer[0] & 0b10000000) > 0
-
-	// Remainder of this byte is reserved
-	res2 := buffer[0] >> 1
-	buffer = buffer[1:]
-
-	// Link is 1 byte
-	link := buffer[0]
-	buffer = buffer[1:]
-
-	// Number of RTokens is 1 byte
-	numRTokens := buffer[0]
-	buffer = buffer[1:]
-
-	// Reserved 2 bytes
-	res3 := buffer[0:2]
-	buffer = buffer[2:]
-
+// parse fills the rkeyPair fields from the buffer
+func (r *rkeyPair) parse(buffer []byte) {
 	// RKey/RToken pairs are 16 bytes and consist of:
 	// * Reference Key (4 bytes)
 	// * New RKey (4 bytes)
 	// * New Virtual Address (8 bytes)
-	getRKeyPair := func() (uint32, uint32, uint64) {
-		referenceRKey := binary.BigEndian.Uint32(buffer[0:4])
-		newRKey := binary.BigEndian.Uint32(buffer[4:8])
-		newVirtualAddr := binary.BigEndian.Uint64(buffer[8:16])
-		return referenceRKey, newRKey, newVirtualAddr
+	r.referenceRKey = binary.BigEndian.Uint32(buffer[0:4])
+	r.newRKey = binary.BigEndian.Uint32(buffer[4:8])
+	r.newVAddr = binary.BigEndian.Uint64(buffer[8:16])
+}
+
+// String converts the rkeyPair to a string
+func (r *rkeyPair) String() string {
+	rFmt := "[Reference RKey: %d, New RKey: %d, New Virtual Address: %#x]"
+	return fmt.Sprintf(rFmt, r.referenceRKey, r.newRKey, r.newVAddr)
+}
+
+// addLinkCont stores a LLC add link continuation message
+type addLinkCont struct {
+	typ        uint8
+	length     uint8
+	res1       byte
+	reply      bool
+	res2       byte
+	link       uint8
+	numRTokens uint8
+	res3       [2]byte
+	rkeyPairs  [2]rkeyPair
+	res4       []byte
+}
+
+// parse fills the addLinkCont fields from the LLC add link continuation
+// message in buffer
+func (a *addLinkCont) parse(buffer []byte) {
+	// Message type is 1 byte
+	a.typ = buffer[0]
+	buffer = buffer[1:]
+
+	// Message length is 1 byte, should be equal to 44
+	a.length = buffer[0]
+	buffer = buffer[1:]
+
+	// Reserved 1 byte
+	a.res1 = buffer[0]
+	buffer = buffer[1:]
+
+	// Reply is first bit in this byte
+	a.reply = (buffer[0] & 0b10000000) > 0
+
+	// Remainder of this byte is reserved
+	a.res2 = buffer[0] >> 1
+	buffer = buffer[1:]
+
+	// Link is 1 byte
+	a.link = buffer[0]
+	buffer = buffer[1:]
+
+	// Number of RTokens is 1 byte
+	a.numRTokens = buffer[0]
+	buffer = buffer[1:]
+
+	// Reserved 2 bytes
+	copy(a.res3[:], buffer[0:2])
+	buffer = buffer[2:]
+
+	// RKey/RToken pairs are each 16 bytes
+	// parse
+	// * first RKey/RToken pair
+	// * second RKey/RToken pair (can be all zero)
+	for _, pair := range a.rkeyPairs {
+		pair.parse(buffer)
+		buffer = buffer[16:]
 	}
 
-	// first RKey/RToken pair
-	referenceRKey1, newRKey1, newVirtualAddr1 := getRKeyPair()
-	buffer = buffer[16:]
-
-	// second RKey/RToken pair (can be all zero)
-	referenceRKey2, newRKey2, newVirtualAddr2 := getRKeyPair()
-	buffer = buffer[16:]
-
 	// Rest of message is reserved
-	res4 := buffer[:]
+	a.res4 = buffer[:]
+}
+
+// String converts the add link continuation message to a string
+func (a *addLinkCont) String() string {
+	var pairs string
+
+	// convert RKey pairs
+	for i, pair := range a.rkeyPairs {
+		pairs = fmt.Sprintf("RKey Pair %d: %s, ", i+1, pair)
+	}
 
 	aFmt := "LLC Add Link Continuation: Type: %d, Length: %d, " +
 		"Reserved: %#x, Reply: %t, Reserved: %#x, Link: %d, " +
-		"Number of RTokens: %d, Reserved: %#x, " +
-		"Reference RKey 1: %d, New RKey 1: %d, " +
-		"New Virtual Address 1: %#x, Reference RKey 2: %d, " +
-		"New RKey 2: %d, New Virtual Address 2: %#x, Reserved : %#x\n"
-	fmt.Printf(aFmt, typ, length, res1, reply, res2, link, numRTokens,
-		res3, referenceRKey1, newRKey1, newVirtualAddr1,
-		referenceRKey2, newRKey2, newVirtualAddr2, res4)
+		"Number of RTokens: %d, Reserved: %#x, %sReserved : %#x\n"
+	return fmt.Sprintf(aFmt, a.typ, a.length, a.res1, a.reply, a.res2,
+		a.link, a.numRTokens, a.res3, pairs, a.res4)
+}
+
+// parseAddLinkCont parses the LLC add link continuation message in buffer
+func parseAddLinkCont(buffer []byte) {
+	var addCont addLinkCont
+	addCont.parse(buffer)
+	fmt.Println(addCont)
 }
 
 // parseDeleteLink parses the LLC delete link message in buffer
