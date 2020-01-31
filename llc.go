eb857c580d5d177afb02c8359f51cb66b38a575c
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-31 02:46:44 +0100

    add pcap file support
    Add support for reading packets from a pcap file.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- llc.go
+++ llc.go
@@ -16,6 +16,7 @@ import (
 // variable definitions
 var (
 	// pcap settings
+	pcapFile    = flag.String("f", "", "the pcap file to read")
 	pcapDevice  = flag.String("i", "eth0", "the interface to listen on")
 	pcapPromisc = flag.Bool("promisc", true, "promiscuous mode")
 	pcapSnaplen = flag.Int("snaplen", 2048, "pcap snaplen")
@@ -1156,16 +1157,29 @@ func parse(packet gopacket.Packet) {
 
 // listen captures packets on the network interface and parses them
 func listen() {
-	// open device */
-	pcapHandle, pcapErr := pcap.OpenLive(*pcapDevice, int32(*pcapSnaplen),
-		*pcapPromisc, pcap.BlockForever)
+	// open pcap handle
+	var pcapHandle *pcap.Handle
+	var pcapErr error
+	var startText string
+	if *pcapFile == "" {
+		// open device
+		pcapHandle, pcapErr = pcap.OpenLive(*pcapDevice,
+			int32(*pcapSnaplen), *pcapPromisc, pcap.BlockForever)
+		startText = fmt.Sprintf("Listening on interface %s:\n",
+			*pcapDevice)
+	} else {
+		// open pcap file
+		pcapHandle, pcapErr = pcap.OpenOffline(*pcapFile)
+		startText = fmt.Sprintf("Reading packets from file %s:\n",
+			*pcapFile)
+	}
 	if pcapErr != nil {
 		log.Fatal(pcapErr)
 	}
 	defer pcapHandle.Close()
 
 	// Use the handle as a packet source to process all packets
-	fmt.Printf("Starting to listen on interface %s.\n", *pcapDevice)
+	fmt.Println(startText)
 	packetSource := gopacket.NewPacketSource(pcapHandle,
 		pcapHandle.LinkType())
 	for packet := range packetSource.Packets() {
