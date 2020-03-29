c3b709704b7f89300cb2950de2f80e258d2cb295
hwipl <33433250+hwipl@users.noreply.github.com>
2020-03-29 16:38:58 +0200

    add internal/llc/cdc_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/llc/cdc_test.go
@@ -0,0 +1,75 @@
+package llc
+
+import (
+	"encoding/hex"
+	"log"
+	"strings"
+	"testing"
+)
+
+func TestCDC(t *testing.T) {
+	var want, got string
+
+	// create bytes of a message
+	msgHex := "00000000  fe 2c 00 01 00 00 00 03  " +
+		"00 00 00 00 00 00 00 0d  |.,..............|\n" +
+		"00000010  00 00 00 00 00 00 00 00  " +
+		"00 00 00 00 00 00 00 00  |................|\n" +
+		"00000020  00 00 00 00 00 00 00 00  " +
+		"00 00 00 00              |............|\n"
+	msg := "fe 2c 00 01 00 00 00 03  00 00 00 00 00 00 00 0d" +
+		"00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00" +
+		"00 00 00 00 00 00 00 00  00 00 00 00"
+	bytes, err := hex.DecodeString(strings.Join(strings.Fields(msg), ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	cdc := parseCDC(bytes)
+
+	// test String()
+	want = "CDC: Type: 254, Length 44, Sequence Number: 1, " +
+		"Alert Token: 3, Producer Wrap: 0, Producer Cursor: 13, " +
+		"Consumer Wrap: 0, Consumer Cursor: 0, " +
+		"Writer Blocked: false, Urgent Data Pending: false, " +
+		"Urgent Data Present: false, " +
+		"Request for Consumer Cursor Update: false, " +
+		"Failover Validation: false, Sending Done: false, " +
+		"Peer Connection Closed: false, Abnormal Close: false\n"
+	got = cdc.String()
+	if got != want {
+		t.Errorf("cdc.String() = %s; want %s", got, want)
+	}
+
+	// test reserved()
+	want = "CDC: Type: 254, Length 44, Sequence Number: 1, " +
+		"Alert Token: 3, Reserved: 0x0000, Producer Wrap: 0, " +
+		"Producer Cursor: 13, Reserved: 0x0000, Consumer Wrap: 0, " +
+		"Consumer Cursor: 0, Writer Blocked: false, " +
+		"Urgent Data Pending: false, Urgent Data Present: false, " +
+		"Request for Consumer Cursor Update: false, " +
+		"Failover Validation: false, Reserved: 0x0, " +
+		"Sending Done: false, Peer Connection Closed: false, " +
+		"Abnormal Close: false, " +
+		"Reserved: 0x00000000000000000000000000000000000000\n"
+	got = cdc.reserved()
+	if got != want {
+		t.Errorf("cdc.reserved() = %s; want %s", got, want)
+	}
+
+	// test hex()
+	want = msgHex
+	got = cdc.hex()
+	if got != want {
+		t.Errorf("cdc.hex() = %s; want %s", got, want)
+	}
+
+	// test getType()
+	wantType := typeCDC
+	gotType := cdc.getType()
+	if gotType != wantType {
+		t.Errorf("cdc.getType() = %d; want %d", gotType, wantType)
+	}
+
+}
