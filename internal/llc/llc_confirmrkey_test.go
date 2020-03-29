8584d1a859e89c6166c4cce469a0f528e2551800
hwipl <33433250+hwipl@users.noreply.github.com>
2020-03-29 16:28:39 +0200

    add internal/llc/llc_confirmrkey_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/llc/llc_confirmrkey_test.go
@@ -0,0 +1,72 @@
+package llc
+
+import (
+	"encoding/hex"
+	"log"
+	"strings"
+	"testing"
+)
+
+func TestConfirmRKey(t *testing.T) {
+	var want, got string
+
+	// create bytes of a message
+	msgHex := "00000000  06 2c 00 00 00 00 00 15  " +
+		"5d 00 00 00 0e ef 4d 00  |.,......].....M.|\n" +
+		"00000010  00 00 00 00 00 00 00 00  " +
+		"00 00 00 00 00 00 00 00  |................|\n" +
+		"00000020  00 00 00 00 00 00 00 00  " +
+		"00 00 00 00              |............|\n"
+	msg := "06 2c 00 00 00 00 00 15  5d 00 00 00 0e ef 4d 00" +
+		"00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00" +
+		"00 00 00 00 00 00 00 00  00 00 00 00"
+	bytes, err := hex.DecodeString(strings.Join(strings.Fields(msg), ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	c := parseConfirmRKey(bytes)
+
+	// test String()
+	want = "LLC Confirm RKey: Type: 6, Length: 44, Reply: false, " +
+		"Negative Response: false, Configuration Retry: false, " +
+		"Number of Tokens: 0, This RKey: 5469, " +
+		"This VAddr: 0xeef4d0000, Other Link RMB 1: " +
+		"[Link: 0, RKey: 0, Virtual Address: 0x0], " +
+		"Other Link RMB 2: [Link: 0, RKey: 0, " +
+		"Virtual Address: 0x0]\n"
+	got = c.String()
+	if got != want {
+		t.Errorf("c.String() = %s; want %s", got, want)
+	}
+
+	// test reserved()
+	want = "LLC Confirm RKey: Type: 6, Length: 44, Reserved: 0x0, " +
+		"Reply: false, Reserved: 0x0, Negative Response: false, " +
+		"Configuration Retry: false, Reserved: 0x0, " +
+		"Number of Tokens: 0, This RKey: 5469, " +
+		"This VAddr: 0xeef4d0000, Other Link RMB 1: " +
+		"[Link: 0, RKey: 0, Virtual Address: 0x0], " +
+		"Other Link RMB 2: [Link: 0, RKey: 0, " +
+		"Virtual Address: 0x0], Reserved: 0x0\n"
+	got = c.reserved()
+	if got != want {
+		t.Errorf("c.reserved() = %s; want %s", got, want)
+	}
+
+	// test hex()
+	want = msgHex
+	got = c.hex()
+	if got != want {
+		t.Errorf("c.hex() = %s; want %s", got, want)
+	}
+
+	// test getType()
+	wantType := typeConfirmRKey
+	gotType := c.getType()
+	if gotType != wantType {
+		t.Errorf("c.getType() = %d; want %d", gotType, wantType)
+	}
+
+}
