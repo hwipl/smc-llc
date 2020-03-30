8d16cb7cea58965acc06f2b7a1dc1d9af00d9a61
hwipl <33433250+hwipl@users.noreply.github.com>
2020-03-30 16:36:11 +0200

    add internal/llc/llc_confirmlink_test.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/llc/llc_confirmlink_test.go
@@ -0,0 +1,68 @@
+package llc
+
+import (
+	"encoding/hex"
+	"log"
+	"strings"
+	"testing"
+)
+
+func TestConfirmLink(t *testing.T) {
+	var want, got string
+
+	// create bytes of a message
+	msgHex := "00000000  01 2c 00 03 98 03 9b ab  " +
+		"cd ef fe 80 00 00 00 00  |.,..............|\n" +
+		"00000010  00 00 9a 03 9b ff fe ab  " +
+		"cd ef 00 01 07 00 00 01  |................|\n" +
+		"00000020  00 00 02 00 00 00 00 00  " +
+		"00 00 00 00              |............|\n"
+	msg := "01 2c 00 03 98 03 9b ab  cd ef fe 80 00 00 00 00" +
+		"00 00 9a 03 9b ff fe ab  cd ef 00 01 07 00 00 01" +
+		"00 00 02 00 00 00 00 00  00 00 00 00"
+	bytes, err := hex.DecodeString(strings.Join(strings.Fields(msg), ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse message
+	c := parseConfirm(bytes)
+
+	// test String()
+	want = "LLC Confirm Link: Type: 1, Length: 44, Reply: false, " +
+		"Sender MAC: 98:03:9b:ab:cd:ef, " +
+		"Sender GID: fe80::9a03:9bff:feab:cdef, " +
+		"Sender QP: 263, Link: 0, Sender Link UserID: 65536, " +
+		"Max Links: 2\n"
+	got = c.String()
+	if got != want {
+		t.Errorf("c.String() = %s; want %s", got, want)
+	}
+
+	// test reserved()
+	want = "LLC Confirm Link: Type: 1, Length: 44, Reserved: 0x0, " +
+		"Reply: false, Reserved: 0x3, " +
+		"Sender MAC: 98:03:9b:ab:cd:ef, " +
+		"Sender GID: fe80::9a03:9bff:feab:cdef, " +
+		"Sender QP: 263, Link: 0, Sender Link UserID: 65536, " +
+		"Max Links: 2, Reserved: 0x000000000000000000\n"
+	got = c.reserved()
+	if got != want {
+		t.Errorf("c.reserved() = %s; want %s", got, want)
+	}
+
+	// test hex()
+	want = msgHex
+	got = c.hex()
+	if got != want {
+		t.Errorf("c.hex() = %s; want %s", got, want)
+	}
+
+	// test getType()
+	wantType := typeConfirmLink
+	gotType := c.getType()
+	if gotType != wantType {
+		t.Errorf("c.getType() = %d; want %d", gotType, wantType)
+	}
+
+}
