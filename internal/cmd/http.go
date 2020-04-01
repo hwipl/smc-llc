3687792c0b1b6a022c42e6867fa4df2e1321b824
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-01 16:03:59 +0200

    move http code into external module
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/http.go
+++ internal/cmd/http.go
@@ -1,31 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-
 	"github.com/hwipl/smc-go/pkg/util"
 )
 
-var (
-	httpBuffer util.Buffer
-)
-
-// printHttp prints the output stored in buffer to http clients
-func printHTTP(w http.ResponseWriter, r *http.Request) {
-	b := httpBuffer.CopyBuffer()
-	if _, err := io.Copy(w, b); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-}
-
 // setHTTPOutput sets the standard output to http and starts a http server
 func setHTTPOutput() {
-	stdout = &httpBuffer
-	stderr = &httpBuffer
-
-	http.HandleFunc("/", printHTTP)
-	go http.ListenAndServe(*httpListen, nil)
+	h := util.StartHTTPOutput(*httpListen)
+	stdout = &h.Buffer
+	stderr = &h.Buffer
 }
